e2f463eb0e6e9666ac970dddb72bbfc3a07f7103
TSAI-CHANCHANG <czz861924@gmail.com>
2017-06-15 09:55:52 +0800

    Update register_file.v

--- multiCPU/CPUcode/register_file.v
+++ multiCPU/CPUcode/register_file.v
@@ -38,9 +38,6 @@ always @(posedge clk or posedge rst) begin
  if (rst==1) begin
 	for (i=1;i<32;i=i+1)
 	register[i]<=0;
-	register[1]<=1;
-	register[2]<=2;
-	register[3]<=3;
  end 
  else begin
 	if ((reg_W_addr!=0)&&(reg_we==1))
